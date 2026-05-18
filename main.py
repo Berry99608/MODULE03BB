6c85c1ad7fea2b62ee1332182b338727930bb817
BB20556 <bouchraberrissoul@gmail.com>
2026-05-18 14:28:09 -0400

    send the read me

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
-
+#definir la fonction d'Importation des mots
+#le fichier doit etre enregistrer dans le dossier du projet.
 def mot_aleatoire():
     with open("mots_pendu.txt") as fichier:
         mots = fichier.readlines()
