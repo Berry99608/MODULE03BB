b49f52818e4415ac8b99c8fed4fa9f125a531dff
BB20556 <bouchraberrissoul@gmail.com>
2026-05-18 14:46:37 -0400

    mise a jour fonction pour enlever les accents

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import random
+#definir la fonction qui enleve les accents
+def enlever_accents(mot):
+    accents ="àâäéèêëîïôöùûüç"
+    sans_accents ="aaaeeeeiioouuuc"
+    resultat = ""
+    for c in mot:
+        if c in accents:
+            resultat += sans_accents[accents.index(c)]
+        else:
+            resultat += c
+    return resultat
 #definir la fonction d'Importation des mots
 #le fichier doit etre enregistrer dans le dossier du projet.
 def mot_aleatoire():
     with open("mots_pendu.txt") as fichier:
         mots = fichier.readlines()
-    return random.choice(mots).strip()
+    return enlever_accents(random.choice(mots).strip())
 
-print("Mot sélectionné :", mot_aleatoire())
+print(mot_aleatoire())
