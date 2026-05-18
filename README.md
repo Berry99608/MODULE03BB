3876dcc18c8b1465651a56e38ad9a6ff055981eb
BB20556 <bouchraberrissoul@gmail.com>
2026-05-18 18:28:26 -0400

    rajouter des lignes de conversation et des commentaires

---
+++ README.md
@@ -0,0 +1,11 @@
+Ce projet est un jeu du pendu développé en Python.  
+Le joueur doit deviner un mot choisi aléatoirement par la machine.
+Fonctionnalités
+
+- Sélection aléatoire d’un mot depuis un fichier `mots_pendu.txt`
+- Suppression automatique des accents (é → e, à → a, etc.)
+- 6 chances par partie
+- Affichage du mot avec `_` pour les lettres non trouvées
+- Indice donné à la dernière chance
+- Système de victoire / défaite
+- Possibilité de rejouer
--- main.py
+++ main.py
@@ -4,11 +4,11 @@ def enlever_accents(mot):
     accents ="àâäéèêëîïôöùûüçÀÂÄÉÈÊËÎÏÔÖÙÛÜÇ"
     sans_accents="aaaeeeeiioouuucAAAEEEEIIOOUUUC"
     resultat = ""
-    for c in mot:
-        if c in accents:
-            resultat += sans_accents[accents.index(c)]
+    for d in mot:
+        if d in accents:
+            resultat += sans_accents[accents.index(d)]
         else:
-            resultat += c
+            resultat += d
     return resultat
 #definir la fonction d'Importation des mots
 
@@ -35,11 +35,12 @@ def afficher(mot, lettres_du_mot):
 #definir la fonction du jeu
 def jouer():
     mot=enlever_accents(mot_aleatoire())
-    lettres_du_mot=[]
-    lettres_rentre=[]# une variable pour prendre les lettres suggeree en note et ne pas les suggerer dans lindice
+    lettres_du_mot=[] # lettres correctes
+    lettres_rentre=[]# une variable pour prendre les lettres suggerees en note et ne pas les suggerer dans l'indice
     chance=6
     while chance>0:
         print (" le mot : ",afficher(mot,lettres_du_mot),"le nombre de chance: ",chance)
+        # Indice (dernière chance)
         if chance == 1:
             pas_dans_le_mot = []
             alphabet="abcdefghijklmnopqrstuvwxyz"
@@ -50,26 +51,30 @@ def jouer():
                 i += 1
             if len(pas_dans_le_mot) > 0:
                 print("Indice : une lettre qui n'est pas dans le mot :", random.choice(pas_dans_le_mot))
-        lettre_a_renter=input("entrez une lettre :")
+        # Entrée utilisateur
+        lettre_a_renter=input("Entrez une lettre :")
         lettres_rentre.append(lettre_a_renter)
         if lettre_a_renter in mot:
             lettres_du_mot.append(lettre_a_renter)
+            print("la lettre:",lettre_a_renter,"est dans le mot.")
         else:
             chance -= 1
+        # Condition de victoire
         if "_" not in afficher(mot,lettres_du_mot):
-            print("vous avez gagne , le mot est affectivement ",mot)
+            print("Bravo ! Vous avez gagne , le mot est effectivement ",mot)
             return
-    print("vous avez perdu, le mot est ",mot)
+    # Défaite
+    print("Vous avez perdu! le mot est ",mot)
 
 #la fonction pour commencer le jeu, et le choix de continuer ou pas
 def boucle_de_jeu():
     print("Bienvenue au jeu du pendu :")
-    print("La machine choisira un mot alleatoire de la liste fournie , et vous devez le deviner.")
+    print("La machine choisira un mot aleatoire de la liste fournie , et vous devez le deviner.")
     while True:
         jouer()
         reponse=input("Est ce que vous aimeriez rejouer ou non : repondez par oui ou non ")
         if reponse=="non":
-            print("Merci !")
+            print("Merci d'avoir joué !")
             break
         elif reponse!="oui":
             reponse=input("rentrez une reponse valide")
